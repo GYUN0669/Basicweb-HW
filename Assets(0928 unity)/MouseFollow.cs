56d835c7b638dccffcb6e77fad2c3cdab095d2c9
GYUN0669 <zktlditm75@gmail.com>
2022-09-28 20:14:52 +0900

    22.09.28 수업 내용

---
+++ Assets(0928 unity)/MouseFollow.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MouseFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float halfW = Screen.width / 2f;
+        float halfH = Screen.width / 3f;
+
+        transform.position = new Vector3 ((Input.mousePosition.x - halfW)/halfW, 0, (Input.mousePosition.x - halfH) / halfH);
+        Debug.Log(Input.mousePosition.x);
+    }
+}
