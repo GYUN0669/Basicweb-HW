adc569c2ded202b24b91625afcd1ff0237f2579f
GYUN0669 <zktlditm75@gmail.com>
2022-10-28 15:26:20 +0900

    22.10.28 소분설

---
+++ goal_control.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class goal_control : MonoBehaviour
+{   
+    private bool is_collided = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionStay(Collision other)
+    {
+        this.is_collided = true;
+        other.transform.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+    }
+    
+
+    void OnGUI()
+    {
+        if (is_collided)
+            GUI.Label(new Rect(Screen.width / 2 - 30, 80, 100, 20), "Success!!!");
+    }
+}
