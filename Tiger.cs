4e2befca259310ecee09322c5cf5abee4da8beea
GYUN0669 <zktlditm75@gmail.com>
2022-12-09 11:58:21 +0900

    2022.12.09

--- Tiger.cs
+++ Tiger.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Tiger : MonoBehaviour
 {
     public float jump_power = 0;
-    public float speed = 5f;
+    public float speed = 2.5f;
     public bool isGround = true;
+    private float time = 0; // 시간 변수 선언
+    public Texture2D icon = null;
+    public int jump = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -20,18 +24,23 @@ public class Tiger : MonoBehaviour
         if (Input.GetButtonDown("Jump") && isGround)
         {
             GetComponent<Rigidbody>().velocity = new Vector3(0, jump_power, 0);
-
+            this.GetComponent<AudioSource>().Play();
             isGround = false;
-
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Translate(0, 0, speed * Time.deltaTime);
+            transform.Translate(-speed * Time.deltaTime, 0,0);
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Translate(0, 0, -speed * Time.deltaTime);
+            transform.Translate(speed * Time.deltaTime, 0, 0);
         }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            SceneManager.LoadScene("Main");
+        }
+
+        time += Time.deltaTime;
     }
     void OnCollisionEnter(Collision collision)
     {
@@ -41,5 +50,12 @@ public class Tiger : MonoBehaviour
             // isGround를 true로 변경
             isGround = true;
         }
+    }
+    void OnGUI()
+    {
+        GUI.DrawTexture(new Rect(700, 0, 64, 64), icon);
+        GUI.Label(new Rect(770, 10, 128, 32), "시간:");
+        GUI.Label(new Rect(810, 10, 128, 32), time.ToString()); // 게임 진행 시간을 표시해줌.
+
     }
 }
